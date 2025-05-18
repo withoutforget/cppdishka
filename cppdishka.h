7c2109e13ac75f922b806b925022fe3fb7627fad
Roman Kovach <89633210+withoutforget@users.noreply.github.com>
2025-05-18 03:31:08 +0300

    updated sscopes

--- cppdishka.h
+++ cppdishka.h
@@ -14,7 +14,7 @@ concept is_any_of = (... || std::is_same_v<T, Ts>);
 
 
 template<typename ... Ts>
-using depends = std::tuple<Ts...>;
+using depends = std::tuple<std::unique_ptr<Ts>...>;
 
 template<typename T>
 struct provider_t {};
@@ -22,7 +22,7 @@ struct provider_t {};
 template<typename ... Ts>
 class Provider {
 public:
-    depends<std::unique_ptr<Ts>...> _deps;
+    depends<Ts...> _deps;
 public:
     template<typename T>
     bool is_provided(provider_t<T>) const {
@@ -37,7 +37,8 @@ template<typename T>
 concept is_injectable = requires(T x) { typename T::depends_t; };
 
 template<class ProviderT, class _Type, class ... Ts>
-void inject_type_impl(const ProviderT& p, std::tuple<Ts...>& t, std::source_location loc = std::source_location::current()) {
+void inject_type_impl(ProviderT& p, std::tuple<Ts...>& t, std::source_location loc = std::source_location::current()) {
+    if (std::get<_Type>(t) != nullptr) return;
     using Type = typename _Type::element_type;
     if constexpr (is_injectable<Type>)
     {
@@ -48,7 +49,7 @@ void inject_type_impl(const ProviderT& p, std::tuple<Ts...>& t, std::source_loca
 }
 
 template<class ProviderT, class ... Ts>
-void inject_type(const ProviderT& p, std::tuple<Ts...>& t) {
+void inject_type(ProviderT& p, std::tuple<Ts...>& t) {
     (inject_type_impl<ProviderT, Ts, Ts...>(p, t), ...);
 }
 
@@ -70,30 +71,30 @@ std::tuple<Ts...> reconvert_tuple(std::tuple<std::unique_ptr<Ts>...>& tuple) {
 }
 
 template<is_injectable T, class ProviderT>
-T inject(const ProviderT& provider) {
-    auto depends = init_tuple((typename T::depends_t*)(nullptr));
+T inject(ProviderT& provider) {
+    auto& depends = provider._deps;
     inject_type(provider, depends);
-    return T{reconvert_tuple(depends)};
+    return T{depends};
 }
 
-#define DI_TN(T1) using depends_t = depends<T1>; 
-#define DI_TN2(T1,T2) using depends_t = depends<T1,T2>; 
-#define DI_TN3(T1,T2,T3) using depends_t = depends<T1,T2,T3>; 
-#define DI_TN4(T1,T2,T3,T4) using depends_t = depends<T1,T2,T3,T4>; 
+#define DI_TN(T1) using depends_t = cppdishka::depends<T1>; 
+#define DI_TN2(T1,T2) using depends_t =  cppdishka::depends<T1,T2>; 
+#define DI_TN3(T1,T2,T3) using depends_t =  cppdishka::depends<T1,T2,T3>; 
+#define DI_TN4(T1,T2,T3,T4) using depends_t = cppdishka::depends<T1,T2,T3,T4>; 
 
-#define DI_CT1(N1,T1) N1(std::get<T1>(depends))
+#define DI_CT1(N1,T1) N1(*std::get<std::unique_ptr<T1>>(depends))
 #define DI_CT2(N1,T1,N2,T2) DI_CT1(N1, T1), DI_CT1(N2,T2)
 #define DI_CT3(N1,T1,N2,T2,N3,T3) DI_CT1(N1, T1), DI_CT1(N2,T2), DI_CT1(N3,T3)
 
 #define DI1(Class, N1, T1) DI_TN(T1) \
-        Class (depends_t depends) : DI_CT1(N1, T1) { }
+        Class (depends_t& depends) : DI_CT1(N1, T1) { }
 #define DI2(Class, N1, T1, N2, T2) DI_TN2(T1,T2) \
-        Class (depends_t depends) : DI_CT2(N1, T1, N2, T2) { }
+        Class (depends_t& depends) : DI_CT2(N1, T1, N2, T2) { }
 #define DI3(Class, N1, T1, N2, T2, N3, T3) DI_TN3(T1,T2,T3) \
-        Class (depends_t depends) : DI_CT3(N1, T1, N2, T2,N3,T3) { }
+        Class (depends_t& depends) : DI_CT3(N1, T1, N2, T2,N3,T3) { }
 
-#define request_provide(T, VAL) decltype(auto) provide(cppdishka::provider_t<T>) const { return VAL ; }
-#define app_provide(T, VAL) decltype(auto) provide(cppdishka::provider_t<T>) const { \
-     if (val = std::get<std::unique_ptr<T>>(this->_deps); val == nullptr) std::get<std::unique_ptr<T>>(this->_deps) = VAL ; \
-     return std::get<std::unique_ptr<T>>(this->_deps).get(); }
+#define request_provide(T, VAL) decltype(auto) provide(cppdishka::provider_t<T>) { return VAL ; }
+#define app_provide(T, VAL) decltype(auto) provide(cppdishka::provider_t<T>) { \
+    auto& val = std::get<std::unique_ptr<T>>(this->_deps); if (val == nullptr) val = std::make_unique<T>(VAL); \
+    return *val; }
 }
--- main.cpp
+++ main.cpp
@@ -1,13 +1,43 @@
 #include <iostream>
 #include <string>
-#include <cppdishka.h>
+#include <format>
+#include <source_location>
+#include "cppdishka.h"
+template<typename T>
+struct ConsoleClass {
+    const std::string m_output;
+    ConsoleClass(const std::string& output, std::source_location l = std::source_location::current()) : m_output(output) {
+        std::cout << std::format("{} ({})\n\n", m_output, l.function_name());
+    }
+    ConsoleClass(const ConsoleClass& rhs,std::source_location l = std::source_location::current()) : m_output(rhs.m_output) {
+        std::cout << std::format("{} ({})\n\n", m_output, l.function_name());
+    }
+    ~ConsoleClass() {
+        std::cout << std::format("~{}\n", m_output);
+    }
+};
 
-class GProvider : public cppdishka::Provider<int, float> {
+class GProvider : public cppdishka::Provider<ConsoleClass<int>, ConsoleClass<float>> {
 public:
-    request_provide(int, 14)
-    app_provide(float, 14)
+    request_provide(ConsoleClass<int>, ConsoleClass<int>("Int"))
+    app_provide(ConsoleClass<float>, ConsoleClass<float>("Float"))
+};
+
+struct A {
+    DI2(A,
+    a, ConsoleClass<int>,
+    b, ConsoleClass<float>
+    )
+
+
+    ConsoleClass<int>& a;
+    ConsoleClass<float>& b;
 };
 
 int main() {
-
+    GProvider provider;
+    for (volatile size_t i = 0; i < 10; i++) {
+        volatile auto obj = cppdishka::inject<A>(provider);
+        std::cout << std::format("test ptr = {}\n", const_cast<void *>(reinterpret_cast<volatile void*>(&obj)));
+    }
 }
